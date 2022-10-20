4917457aeb418104dcb51fde7cef53ff99c2e342
AlejandraJimenezS <90272365+AlejandraJimenezS@users.noreply.github.com>
2022-10-20 17:38:04 -0500

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+author: Alejandra Jiménez Soto
+date: 10/20/2022
+
+"""
+
+a=4
+b=20
+c=27
+
+if(a>b):
+    if (a>c):
+        print("a es el mayor de todos")
+    else:
+        print("c es el mayor de todos")
+else:
+    if(b>c):
+        print("b es el mayor de todos")
+    else:
+        print("c es el mayor que todos")
+"""
+elif(b>c):
+    print("b es el mayor de todos")
+else:
+    print("c es el mayor que todos")
+"""
+"""
+a=input("Escribe un valor: ")
+a=int
+"""
